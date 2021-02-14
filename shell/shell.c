f25ed4797328a8e872f21e34aa316d564fa75b95
ashankk2 <ashankk2@illinois.edu>
2021-02-14 23:49:26 +0000

    Deploying shell

---
+++ shell/shell.c
@@ -0,0 +1,17 @@
+/**
+ * shell
+ * CS 241 - Spring 2021
+ */
+#include "format.h"
+#include "shell.h"
+#include "vector.h"
+
+typedef struct process {
+    char *command;
+    pid_t pid;
+} process;
+
+int shell(int argc, char *argv[]) {
+    // TODO: This is the entry point for your shell.
+    return 0;
+}
