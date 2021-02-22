2abcd742a910e407d1fd4239814551519326b778
Xinshuo Lei <xinshuo3@illinois.edu>
2021-02-22 05:59:21 +0000

    start shell

--- shell/shell.c
+++ shell/shell.c
@@ -5,13 +5,24 @@
 #include "format.h"
 #include "shell.h"
 #include "vector.h"
+#include <signal.h>
+
 
 typedef struct process {
     char *command;
     pid_t pid;
 } process;
 
+void signal_handler(int sig_name) {
+    if(sig_name == SIGINT) {
+
+    } else if (sig_name == SIGCHLD) {
+
+    }
+}
+
 int shell(int argc, char *argv[]) {
     // TODO: This is the entry point for your shell.
+    signal(SIGINT, signal_handler);
     return 0;
 }
