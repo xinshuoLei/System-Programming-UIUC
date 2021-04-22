ba05dd3e9d67e5c0fd852285b789c2cbb6c9803f
Xinshuo Lei <xinshuo3@illinois.edu>
2021-04-22 23:41:54 +0000

    finish mad_mad_access_patterns

--- mad_mad_access_patterns/lookup1.c
+++ mad_mad_access_patterns/lookup1.c
@@ -1,16 +1,76 @@
 /**
+ * partner: haoyul4, xinshuo3
  * mad_mad_access_patterns
  * CS 241 - Spring 2021
  */
 #include "tree.h"
 #include "utils.h"
+#include <stdlib.h>
+#include <string.h>
 /*
   Look up a few nodes in the tree and print the info they contain.
   This version uses fseek() and fread() to access the data.
 
   ./lookup1 <data_file> <word> [<word> ...]
 */
+int find(FILE* fp, char* word, long offset) {
+  if (offset == 0) {
+    return 0;
+  }
+  BinaryTreeNode n;
+  fseek(fp, offset, SEEK_SET);
+  fread(&n, sizeof(BinaryTreeNode), 1, fp);
+  char c;
+  int word_length = 0;
+  while ((c = fgetc(fp)) != '\0' && c != EOF) {
+    word_length++;
+  }
+  fseek(fp, offset + sizeof(BinaryTreeNode), SEEK_SET);
+  char node_word[word_length];
+  fread(node_word, word_length, 1, fp);
+  if (strcmp(word, node_word) == 0){
+      printFound(node_word, n.count, n.price);
+      return 1;
+    }
+    if (strcmp(word, node_word) < 0){
+      if (find(fp,  word, n.left_child)){
+        return 1;
+      }
+    }
+    if (strcmp(word, node_word) > 0){
+      if (find(fp,  word, n.right_child)){
+        return 1;
+      }
+    }
+  return 0;
+}
 
 int main(int argc, char **argv) {
+    if (argc < 3) {
+      printArgumentUsage();
+      exit(1);
+    }
+    char* file_name = argv[1];
+    FILE* fp = fopen(file_name, "r");
+    if (!fp) {
+      openFail(file_name);
+      exit(2);
+    }
+    char buffer[4];
+    fread(buffer, 1, 4, fp);
+    if (strcmp(buffer, "BTRE")) {
+      formatFail(file_name);
+      exit(2);
+    }
+    int i = 2;
+    for (; i < argc; i++) {
+      int check = find(fp, argv[i], (long)4 );
+      if (!check) {
+        printNotFound(argv[i]);
+      }
+    }
+    fclose(fp);
+
+    
     return 0;
 }
--- mad_mad_access_patterns/lookup2.c
+++ mad_mad_access_patterns/lookup2.c
@@ -1,9 +1,18 @@
 /**
+ * partners: haoyul4, xinshuo3
  * mad_mad_access_patterns
  * CS 241 - Spring 2021
+ 
  */
 #include "tree.h"
 #include "utils.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
 
 /*
   Look up a few nodes in the tree and print the info they contain.
@@ -12,6 +21,60 @@
   ./lookup2 <data_file> <word> [<word> ...]
 */
 
+int find(long offset, char* buffer, char* word) {
+  if(offset == 0) {
+    return 0;
+  } 
+  BinaryTreeNode* n = (BinaryTreeNode *) (buffer + offset);
+  if (!strcmp(word, n->word)){
+     printFound(n->word, n->count, n->price);
+     return 1;
+  }
+  if (strcmp(word, n->word) < 0) {
+    if (find(n->left_child, buffer, word)){
+       return 1;
+    }
+  }
+  if (strcmp(word, n->word) > 0) {
+    if (find(n->right_child, buffer, word)){
+       return 1;
+    }
+  }
+  return 0;
+}
 int main(int argc, char **argv) {
+    if (argc < 3) {
+      printArgumentUsage();
+      exit(1);
+    }
+    char* file_name = argv[1];
+    int fd = open(file_name, O_RDONLY);
+    if (fd < 0) {
+      openFail(file_name);
+      exit(2);
+    }
+    struct stat s;
+    if (fstat(fd, &s)) {
+      openFail(file_name);
+      exit(2);
+    }
+    char* buffer = mmap(NULL, s.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (buffer == (void*) -1) {
+      mmapFail(file_name);
+      exit(2);
+    }
+    if (strncmp(buffer, "BTRE", 4)) {
+      formatFail(file_name);
+      exit(2);
+    }
+    int i = 2;
+
+    for (; i < argc; i++) {
+      int check = find((long) 4, buffer, argv[i]);
+      if (!check) {
+        printNotFound(argv[i]);
+      }
+    }
+    close(fd);
     return 0;
 }
