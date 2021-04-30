55b65cf43ef9b9f62f2d99fc743dff8baea8d0b1
Xinshuo Lei <xinshuo3@illinois.edu>
2021-04-30 04:16:28 +0000

    finish scheduler

--- savvy_scheduler/libscheduler.c
+++ savvy_scheduler/libscheduler.c
@@ -1,6 +1,7 @@
 /**
  * savvy_scheduler
  * CS 241 - Spring 2021
+ * partners: haoyul4 peiyuan3
  */
 #include "libpriqueue/libpriqueue.h"
 #include "libscheduler.h"
@@ -21,7 +22,19 @@ typedef struct _job_info {
 
     /* TODO: Add any other information and bookkeeping you need into this
      * struct. */
+     double arrive_time;
+     double remaining_time;
+     double recent_run_time;
+     double required_time;
+     double start_time;
+     int priority;
 } job_info;
+
+int num_process;
+double wait_time;
+double response_time;
+double turnaround_time;
+
 
 void scheduler_start_up(scheme_t s) {
     switch (s) {
@@ -50,6 +63,10 @@ void scheduler_start_up(scheme_t s) {
     priqueue_init(&pqueue, comparision_func);
     pqueue_scheme = s;
     // Put any additional set up code you may need here
+    num_process = 0;
+    wait_time = 0;
+    response_time = 0;
+    turnaround_time = 0;
 }
 
 static int break_tie(const void *a, const void *b) {
@@ -58,7 +75,12 @@ static int break_tie(const void *a, const void *b) {
 
 int comparer_fcfs(const void *a, const void *b) {
     // TODO: Implement me!
-    return 0;
+    job_info *info_a = ((job*)a)->metadata;
+    job_info *info_b = ((job*)b)->metadata;
+    if (info_a->arrive_time < info_b->arrive_time) {
+        return -1;
+    }
+    return 1;
 }
 
 int comparer_ppri(const void *a, const void *b) {
@@ -68,22 +90,53 @@ int comparer_ppri(const void *a, const void *b) {
 
 int comparer_pri(const void *a, const void *b) {
     // TODO: Implement me!
-    return 0;
+    job_info *info_a = ((job*)a)->metadata;
+    job_info *info_b = ((job*)b)->metadata;
+    if ((info_a->priority - info_b->priority) == 0) {
+        return break_tie(a, b);
+    } else if ((info_a->priority - info_b->priority) < 0) {
+        return -1;
+    }
+    return 1;
 }
 
 int comparer_psrtf(const void *a, const void *b) {
     // TODO: Implement me!
-    return 0;
+    job_info *info_a = ((job*)a)->metadata;
+    job_info *info_b = ((job*)b)->metadata;
+    if ((info_a->remaining_time - info_b->remaining_time) == 0) {
+        return break_tie(a, b);
+    } else if ((info_a->remaining_time - info_b->remaining_time)<0){
+        return -1;
+    }
+    return 1;
 }
 
 int comparer_rr(const void *a, const void *b) {
     // TODO: Implement me!
-    return 0;
+    job_info *info_a = ((job*)a)->metadata;
+    job_info *info_b = ((job*)b)->metadata;
+    if ((info_a->recent_run_time - info_b->recent_run_time) == 0) {
+        return break_tie(a, b);
+    } else if ((info_a->recent_run_time - info_b->recent_run_time) < 0) {
+        return -1;
+    }
+    return 1;
 }
 
 int comparer_sjf(const void *a, const void *b) {
     // TODO: Implement me!
-    return 0;
+    job_info *info_a = ((job*)a)->metadata;
+    job_info *info_b = ((job*)b)->metadata;
+    if ((info_a->required_time - info_b->required_time) == 0) {
+        return break_tie(a, b);
+    } else if ((info_a->required_time - info_b->required_time) < 0) {
+        return -1;
+    }
+    return 1;
+    /*
+    int result = info_a->recent_run_time - info_b->recent_run_time;
+    return (result == 0) ? break_tie(a, b) : ((result < 0) ? -1 : 1);*/
 }
 
 // Do not allocate stack space or initialize ctx. These will be overwritten by
@@ -91,15 +144,47 @@ int comparer_sjf(const void *a, const void *b) {
 void scheduler_new_job(job *newjob, int job_number, double time,
                        scheduler_info *sched_data) {
     // TODO: Implement me!
+    job_info *info = malloc(sizeof(job_info));
+    info->id = job_number;
+    info->start_time = -1;
+    info->priority = sched_data->priority;
+    info->arrive_time = time;
+    info->remaining_time = sched_data->running_time;
+    info->recent_run_time = -1;
+    info->required_time = sched_data->running_time;
+    newjob->metadata = info;
+    priqueue_offer(&pqueue, newjob);
 }
 
 job *scheduler_quantum_expired(job *job_evicted, double time) {
     // TODO: Implement me!
-    return NULL;
+    if (!job_evicted) {
+        return priqueue_peek(&pqueue);
+    }
+    // set time for current job
+    job_info* info = job_evicted->metadata;
+    info->recent_run_time = time;
+    info->remaining_time -= 1;
+    if (info->start_time < 0) {
+        info->start_time = time -1;
+    }
+    if (pqueue_scheme == PPRI || pqueue_scheme == PSRTF || pqueue_scheme == RR) {
+        job* curr = priqueue_poll(&pqueue);
+        priqueue_offer(&pqueue, curr);
+        return priqueue_peek(&pqueue);
+    }
+    return job_evicted;
 }
 
 void scheduler_job_finished(job *job_done, double time) {
     // TODO: Implement me!
+    num_process += 1;
+    job_info* j = job_done->metadata;
+    wait_time += time - j->arrive_time - j->required_time;
+    response_time += j->start_time - j->arrive_time;
+    turnaround_time += time - j->arrive_time;
+    free(j);
+    priqueue_poll(&pqueue);
 }
 
 static void print_stats() {
@@ -110,21 +195,34 @@ static void print_stats() {
 
 double scheduler_average_waiting_time() {
     // TODO: Implement me!
-    return 9001;
+    return wait_time/num_process;
 }
 
 double scheduler_average_turnaround_time() {
     // TODO: Implement me!
-    return 9001;
+    return turnaround_time/num_process;
 }
 
 double scheduler_average_response_time() {
     // TODO: Implement me!
-    return 9001;
+    return response_time/num_process;
 }
 
 void scheduler_show_queue() {
     // OPTIONAL: Implement this if you need it!
+    printf("********* priority queue *********\n");
+    for (int i = 0; i < priqueue_size(&pqueue); ++i) {
+        job *curr = priqueue_poll(&pqueue);
+        job_info* info = curr->metadata;
+        printf("id: %d\n", info->id);
+        printf("--priority: %d\n", info->priority);
+        printf("--arrive time: %f\n", info->arrive_time);
+        printf("--starttime: %f\n", info->start_time);
+        printf("--required time: %f\n", info->required_time);
+        printf("--recent run time: %f\n", info->recent_run_time);
+        printf("--remaining time: %f\n", info->remaining_time);
+        priqueue_offer(&pqueue, curr);
+    }
 }
 
 void scheduler_clean_up() {
